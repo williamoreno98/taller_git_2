89c2749b9b9549c0bd5871668ac18f8d6f7cd50f
williamoreno98 <moreno.william9809@gmail.com>
2025-08-07 06:59:58 -0500

    Load dataframe from csv

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     # To do: Completar la función 
+    df = pd.read_csv("datos_energia.csv")
     
+    df['time'] = pd.to_datetime(df['time'])
+    
+    # Establecer la columna de fecha como índice
+    df.set_index('time', inplace=True)
+    
+    return df
 
 # Cargar datos
 data = load_data()
@@ -240,4 +247,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
